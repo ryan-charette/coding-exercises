a9e1b8dc67f3fbc54a12ad7655be9f3ca335b6f8
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-10 10:46:14 -0500

    Create cosine_similarity.py

---
+++ numpy/cosine_similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cosine_similarity(v1, v2):
+	"""
+	Calculate the cosine_similarity of two vectors.
+	Args:
+		vec1 (numpy.ndarray): 1D array representing the first vector.
+		vec2 (numpy.ndarray): 1D array representing the second vector.
+	Returns:
+		The cosine_similarity of the two vectors.
+	"""
+	dot_product = np.dot(v1, v2)
+	norm_v1 = np.linalg.norm(v1)
+	norm_v2 = np.linalg.norm(v2)
+
+	return dot_product / (norm_v1 * norm_v2)
