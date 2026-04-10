7ced61722170c42929599830bd301de9ace923a3
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-09 22:32:00 -0500

    Update scalar_multiply.py
    Formatting

--- python/scalar_multiply.py
+++ python/scalar_multiply.py
@@ -1,9 +1,11 @@
 def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
 	result = []
+	
 	for row in matrix:
 		new_row = []
 		for value in row:
 			product = value * scalar
 			new_row.append(product)
 		result.append(new_row)
+		
 	return result
