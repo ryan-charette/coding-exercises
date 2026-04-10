9bdab67d3618771b05c5a858e8cd55e718eeedf2
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-04-10 10:48:54 -0500

    Update cosine_similarity.py
    Fixed documentation error

--- numpy/cosine_similarity.py
+++ numpy/cosine_similarity.py
@@ -4,8 +4,8 @@ def cosine_similarity(v1, v2):
 	"""
 	Calculate the cosine_similarity of two vectors.
 	Args:
-		vec1 (numpy.ndarray): 1D array representing the first vector.
-		vec2 (numpy.ndarray): 1D array representing the second vector.
+		v1 (numpy.ndarray): 1D array representing the first vector.
+		v2 (numpy.ndarray): 1D array representing the second vector.
 	Returns:
 		The cosine_similarity of the two vectors.
 	"""
