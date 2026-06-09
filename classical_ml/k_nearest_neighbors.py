30bd9da89c9d93a12767342027c0ed515f863023
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-06-08 23:42:29 -0500

    Create k_nearest_neighbors.py

---
+++ classical_ml/k_nearest_neighbors.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def k_nearest_neighbors(points, query_point, k):
+    """
+    Find k nearest neighbors to a query point
+    
+    Args:
+        points: List of tuples representing points [(x1, y1), (x2, y2), ...]
+        query_point: Tuple representing query point (x, y)
+        k: Number of nearest neighbors to return
+    
+    Returns:
+        List of k nearest neighbor points as tuples
+        When distances are tied, points appearing earlier in the input list come first.
+    """
+    pass
