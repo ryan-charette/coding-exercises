6c17b4240900b0f138ed83b2c293bdcdc548916b
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2026-06-08 23:57:26 -0500

    Create batch_normalization.py

---
+++ neural_networks/batch_normalization.py
@@ -0,0 +1,5 @@
+import torch
+
+def batch_normalization(X: torch.Tensor, gamma: torch.Tensor, beta: torch.Tensor, epsilon: float = 1e-5) -> torch.Tensor:
+    """Perform Batch Normalization on a 4D tensor in BCHW format."""
+    pass
